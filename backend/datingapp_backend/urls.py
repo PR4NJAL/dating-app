227587773a018c1e836de57bff6e3213fd7dcc78
PR4NJAL <pranjalbhardwaj.in@gmail.com>
2025-02-23 02:50:59 +0530

    ML Model

--- backend/datingapp_backend/urls.py
+++ backend/datingapp_backend/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import PurityTestResponseViewSet, MyTokenObtainPairView, RegisterView
+from .views import PurityTestResponseViewSet, MyTokenObtainPairView, RegisterView, top_matches
 
 router = DefaultRouter()
 router.register(r'datingapp_backend', PurityTestResponseViewSet, basename='datingapp_backend')
@@ -11,4 +11,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='auth_register'),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('matches/', top_matches, name="matches"),
 ]
--- backend/datingapp_backend/views.py
+++ backend/datingapp_backend/views.py
@@ -4,9 +4,11 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 from .models import Answers
-import json
 from .serializers import AnswersSerializers, UserSerializer, MyTokenObtainPairSerializer
+from .utils import get_top_matches
+import json
 
 class PurityTestResponseViewSet(viewsets.ModelViewSet):
     serializer_class = AnswersSerializers
@@ -49,3 +51,10 @@ class RegisterView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         print("Validation Errors:", serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def top_matches(request):
+    user = request.user
+    matches = get_top_matches(user)
+    return Response(matches)
